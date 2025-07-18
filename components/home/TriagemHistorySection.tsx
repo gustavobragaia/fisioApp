d51e3d6ca1588a48d8fc9d13f7a3b5d454a09ac6
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-07-18 18:00:36 +0100

    fix: limit displayed triagem history to the latest three items

--- components/home/TriagemHistorySection.tsx
+++ components/home/TriagemHistorySection.tsx
@@ -1,8 +1,8 @@
+import { TriagemItem } from "@/types/dashboard";
 import { useRouter } from "expo-router";
 import { FlatList, View } from "react-native";
 import { EmptyState } from "../EmptyState";
 import { TriagemHistoryCard } from "./TriagemHistoryCard";
-import { TriagemItem } from "@/types/dashboard";
 
 export const TriagemHistorySection = ({
   triagemHistory,
@@ -15,7 +15,7 @@ export const TriagemHistorySection = ({
     <View>
       {triagemHistory.length > 0 ? (
         <FlatList
-          data={triagemHistory}
+          data={triagemHistory.slice(0, 3)}
           renderItem={({ item }) => <TriagemHistoryCard item={item} />}
           keyExtractor={(item) => item.id}
           scrollEnabled={false}
